8be9fc5db84bdf7854babc0dba04d78996e275da
gaogao-qwq <gaogaoqwq@gmail.com>
2023-10-20 15:24:35 +0800

    Feat: Update useful key binds

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -10,15 +10,19 @@ keymap.set("i", "jk", "<ESC>") -- esc 改为 jk
 keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- shift + j 向下移动
 keymap.set("v", "K", ":m '<-2<CR>gv=gv") -- shift + k 向上移动
 
+-- 全选
+keymap.set("v", "<C-a>", "<ESC>ggVG")
+
 --- 正常模式 ---
 -- 窗口
 keymap.set("n", "<leader>sv", "<C-w>v") -- 主键 + sv 水平新增窗口
 keymap.set("n", "<leader>sh", "<C-w>s") -- 主键 + sh 垂直新增窗口
 
 -- 内嵌终端
+-- ALT + = 打开或关闭内嵌终端
 keymap.set("n", "<M-=>", ":FloatermToggle<CR>")
 keymap.set("t", "<M-=>", "<c-\\><c-N>:FloatermToggle<CR>")
-keymap.set("t", "<M-->", "<c-\\><c-N>:FloatermKill<CR>")
+keymap.set("t", "<M-=>", "<c-\\><c-N>:FloatermKill<CR>")
 
 -- 移动
 keymap.set("n", "<M-j>", ":+10<CR>")
@@ -26,6 +30,9 @@ keymap.set("n", "<M-k>", ":-10<CR>")
 
 -- 取消高亮
 keymap.set("n", "<leader>nh", ":nohl<CR>")
+
+-- 进入视觉模式并全选
+keymap.set("n", "<C-a>", "ggVG")
 
 --- 插件 ---
 -- nvim-tree
