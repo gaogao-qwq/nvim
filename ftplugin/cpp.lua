eea437b5edab0deaea0913839369e521ab36414f
gaogao-qwq <gaogaoqwq@gmail.com>
2023-10-21 13:55:09 +0800

    Chore: Set different tab sizes for each language by configure ftplugin

---
+++ ftplugin/cpp.lua
@@ -0,0 +1,5 @@
+local bo = vim.bo
+
+bo.tabstop = 4 -- size of a hard tabstop (ts).
+bo.shiftwidth = 4 -- size of an indentation (sw).
+bo.expandtab = false -- always uses spaces instead of tab characters (et).
---
+++ ftplugin/typescript.lua
@@ -0,0 +1,6 @@
+local bo = vim.bo
+
+bo.tabstop = 2 -- size of a hard tabstop (ts).
+bo.shiftwidth = 2 -- size of an indentation (sw).
+bo.expandtab = true -- always uses spaces instead of tab characters (et).
+bo.softtabstop = 2 -- number of spaces a <Tab> counts for. When 0, feature is off (sts).
