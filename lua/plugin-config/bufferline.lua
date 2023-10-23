acd205762e147d2874e7ad89c4f45ec4fa82c696
gaogao-qwq <gaogaoqwq@gmail.com>
2023-10-23 12:20:07 +0800

    Chore: Display buffer id and ordinal label

--- lua/plugin-config/bufferline.lua
+++ lua/plugin-config/bufferline.lua
@@ -1,9 +1,11 @@
-vim.opt.termguicolors = true
-
 require("bufferline").setup {
 	options = {
 		-- 使用 nvim 内置 lsp
 		diagnostics = "nvim_lsp",
+		-- 标签编号
+		numbers = function(opts)
+			return string.format('%s|%s', opts.id, opts.raise(opts.ordinal))
+		end,
 		-- 左侧让出 nvim-tree 位置
 		offsets = {{
 			filetype = "NvimTree",
