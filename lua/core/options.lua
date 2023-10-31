3a8b22cc7ee4dc214ad81afdd19e2f74d6dbb3bd
gaogao-qwq <gaogaoqwq@gmail.com>
2023-10-31 10:31:46 +0800

    Fix: Fix removed nerd font icon

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -48,7 +48,7 @@ vim.cmd[[colorscheme tokyonight-night]]
 -- vim.cmd[[highlight SignColumn guibg=NONE ctermbg=None]]
 
 -- 诊断标志
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+local signs = { Error = " ", Warn = " ", Hint = "󰌶 ", Info = " " }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
