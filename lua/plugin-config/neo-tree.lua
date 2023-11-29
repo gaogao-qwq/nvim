a445f81f369ef38639624be373b582b4b6298563
gaogao-qwq <gaogaoqwq@gmail.com>
2023-11-29 08:27:52 +0800

    Chore: Add source_selector config for neo-tree
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- lua/plugin-config/neo-tree.lua
+++ lua/plugin-config/neo-tree.lua
@@ -6,5 +6,38 @@ require("neo-tree").setup({
 			hide_gitingore = false,
 			hide_hidden = false,
 		}
-	}
+	},
+	source_selector = {
+		winbar = true,
+		statusline = true,
+		show_scrolled_off_parent_node = false,
+		sources = {
+			{
+				source = "filesystem",
+				display_name = " 󰉓 Files "
+			},
+			{
+				source = "buffers",
+				display_name = " 󰈚 Buffers "
+			},
+			{
+				source = "git_status",
+				display_name = " 󰊢 Git "
+			},
+		},
+		content_layout = "start",
+		tabs_layout = "equal",
+		truncation_character = "…",
+		tabs_min_width = nil,
+		tabs_max_width = nil,
+		padding = 0,
+		separator = { left = "▏", right= "▕" },
+		separator_active = nil,
+		show_separator_on_edge = false,
+		highlight_tab = "NeoTreeTabInactive",
+		highlight_tab_active = "NeoTreeTabActive",
+		highlight_background = "NeoTreeTabInactive",
+		highlight_separator = "NeoTreeTabSeparatorInactive",
+		highlight_separator_active = "NeoTreeTabSeparatorActive",
+	},
 })
