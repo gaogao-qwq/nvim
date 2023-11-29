6b590531b8e297b852a57dfb386d4ce2f8cd7d21
gaogao-qwq <gaogaoqwq@gmail.com>
2023-11-29 23:08:56 +0800

    Chore: Disable keybind K to <Cmd>GoDoc<CR> when editing go file with vim-go
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- ftplugin/go.lua
+++ ftplugin/go.lua
@@ -3,3 +3,5 @@ local bo = vim.bo
 bo.tabstop = 4 -- size of a hard tabstop (ts).
 bo.shiftwidth = 4 -- size of an indentation (sw).
 bo.expandtab = false -- always uses spaces instead of tab characters (et).
+
+vim.g.go_doc_keywordprg_enabled = 0 -- Disable keybind K to <Cmd>GoDoc<CR>
