48c9aa6d368b446630ae39e2723aed3a1d7b3708
gaogao-qwq <gaogaoqwq@gmail.com>
2023-12-29 23:58:27 +0800

    Feat: Update jdtls lsp config for better spring dev experience
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -5,11 +5,45 @@ bo.shiftwidth = 4 -- size of an indentation (sw).
 bo.expandtab = true -- always uses spaces instead of tab characters (et).
 bo.softtabstop = 4 -- number of spaces a <Tab> counts for. When 0, feature is off (sts).
 
-local jdtls_bin = string.sub(vim.fn.system('which jdtls'), 0, -2)
+local home = string.sub(vim.fn.system("echo $HOME"), 0, -2)
+local lombok_jar = home .. "/.local/share/eclipse/lombok.jar"
+local jdtls_bin = string.sub(vim.fn.system("readlink -f $(which jdtls)"), 0, -2)
+local jdtls_dir = string.sub(jdtls_bin, 0, -11)
+local jdtls_jar = vim.fn.glob(jdtls_dir .. "/libexec/plugins/org.eclipse.equinox.launcher_*.jar")
+local jdtls_conf = jdtls_dir .. "/libexec/config_mac"
+local root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1])
+local workspace_folder = home .. "/.local/share/eclipse/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
 
 local config = {
-    cmd = { jdtls_bin },
-    root_dir = require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew'}),
+    root_dir = root_dir,
+	settings = {
+		java = {
+			import = {
+				gradle = {
+					annotationProcessing = { enable = true }
+				},
+			},
+		},
+	},
+	init_options = {
+		bundles = {}
+	},
+    cmd = {
+		'java',
+		'-Declipse.application=org.eclipse.jdt.ls.core.id1',
+		'-Dosgi.bundles.defaultStartLevel=4',
+		'-Declipse.product=org.eclipse.jdt.ls.core.product',
+		'-Dlog.protocol=true',
+		'-Dlog.level=ALL',
+		'-Xmx1g',
+		'--add-modules=ALL-SYSTEM',
+		'--add-opens', 'java.base/java.util=ALL-UNNAMED',
+		'--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+		'-javaagent:' .. lombok_jar,
+		'-Xbootclasspath/a:' .. lombok_jar,
+		'-jar', jdtls_jar,
+		'-configuration', jdtls_conf,
+		'-data', workspace_folder,
+	},
 }
-
 require('jdtls').start_or_attach(config)
