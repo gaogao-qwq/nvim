2c5376ae91f6ca4085066b30881c97b4e6e87f3b
gaogao-qwq <gaogaoqwq@gmail.com>
2023-12-31 23:22:53 +0800

    Chore: Add telescope extension for flutter
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- lua/plugin-config/telescope.lua
+++ lua/plugin-config/telescope.lua
@@ -22,4 +22,4 @@ require('telescope').setup({
 require('telescope').load_extension('file_browser')
 require('telescope').load_extension('fzf')
 require('telescope').load_extension('live_grep_args')
-
+require("telescope").load_extension("flutter")
