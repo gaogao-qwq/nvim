e4e6bb23f799146d46ba18fcc523ddce721b130e
gaogao-qwq <gaogaoqwq@gmail.com>
2024-01-02 22:15:15 +0800

    Chore: Update noice config
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- lua/plugin-config/noice.lua
+++ lua/plugin-config/noice.lua
@@ -13,16 +13,25 @@ require("noice").setup({
 			input = {}, -- Used by input()
 		}
 	},
+	messages = {
+		enabled = true, -- enables the Noice messages UI
+		view = "notify", -- default view for messages
+		view_error = "notify", -- view for errors
+		view_warn = "notify", -- view for warnings
+		view_history = "messages", -- view for :messages
+		view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+	},
 	lsp = {
-		override = {
-			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-			["vim.lsp.util.stylize_markdown"] = true,
-			["cmp.entry.get_documentation"] = true,
+		progress = {
+			enabled = false,
 		},
+		hover = {
+			enabled = false,
+		}
 	},
 	presets = {
 		bottom_search = false, -- use a classic bottom cmdline for search
-		command_palette = true, -- position the cmdline and popupmenu together
+		command_palette = false, -- position the cmdline and popupmenu together
 		long_message_to_split = true, -- long messages will be sent to a split
 		inc_rename = false, -- enables an input dialog for inc-rename.nvim
 		lsp_doc_border = false, -- add a border to hover docs and signature help
