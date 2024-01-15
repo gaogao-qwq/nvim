f47e6e7f336e25b671070b5e1ab8276f719eebed
gaogao-qwq <gaogaoqwq@gmail.com>
2024-01-16 06:22:46 +0800

    Chore: Enable flutter-tools widget_guides and color preview
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- lua/plugin-config/flutter-tools.lua
+++ lua/plugin-config/flutter-tools.lua
@@ -1,1 +1,14 @@
-require('flutter-tools').setup {}
+require('flutter-tools').setup {
+	widget_guides = {
+		enabled = true,
+	},
+	lsp = {
+		color = {
+			enabled = true,
+			background = false,
+			foreground = false,
+			virtual_text = true,
+			virtual_text_str = "■",
+		}
+	}
+}
