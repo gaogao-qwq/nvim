14b73038f33fb614fbeb5d18176ec547336ef680
gaogao-qwq <gaogaoqwq@gmail.com>
2024-02-02 00:58:08 +0800

    Fix: Fix just treesitter source file name
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- lua/plugin-config/tree-sitter-just.lua
+++ lua/plugin-config/tree-sitter-just.lua
@@ -1,7 +1,7 @@
 require("nvim-treesitter.parsers").get_parser_configs().just = {
 	install_info = {
 		url = "https://github.com/IndianBoy42/tree-sitter-just", -- local path or git repo
-		files = { "src/parser.c", "src/scanner.cc" },
+		files = { "src/parser.c", "src/scanner.c" },
 		branch = "main",
 		-- use_makefile = true -- this may be necessary on MacOS (try if you see compiler errors)
 	},
