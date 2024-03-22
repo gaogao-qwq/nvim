3268bc2e8aee3e8b1ea1e726756ff06d71a438f5
gaogao-qwq <gaogaoqwq@gmail.com>
2024-03-22 09:16:26 +0800

    Chore: Clear neodev config & let neodev setup before lspconfig
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- lua/plugin-config/neodev.lua
+++ lua/plugin-config/neodev.lua
@@ -1,6 +1,3 @@
 -- IMPORTANT: make sure to setup neodev BEFORE lspconfig
-require("neodev").setup({
-	library = { plugins = { "nvim-dap-ui" }, types = true },
-	-- add any options here, or leave empty to use the default settings
-})
+require("neodev").setup{}
 
