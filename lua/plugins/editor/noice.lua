a2bff333998290fa06fbe5e96210a05181b15b95
gaogao-qwq <gaogaoqwq@gmail.com>
2024-04-04 06:44:07 +0800

    Chore: Disable lsp progress indicator
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- lua/plugins/editor/noice.lua
+++ lua/plugins/editor/noice.lua
@@ -1,7 +1,10 @@
 return {
-	'folke/noice.nvim',
+	"folke/noice.nvim",
 	opts = {
 		lsp = {
+			progress = {
+				enabled = false,
+			},
 			override = {
 				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 				["vim.lsp.util.stylize_markdown"] = true,
