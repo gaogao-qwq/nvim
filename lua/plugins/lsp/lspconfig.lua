929363959fe0fe33a5c352c5e899473c57563bae
gaogao-qwq <gaogaoqwq@gmail.com>
2024-04-08 14:56:39 +0800

    Chore: Update config for clangd
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -51,7 +51,51 @@ return {
 					},
 				},
 			},
+			clangd = {
+				keys = {
+					{ "<leader>cR", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+				},
+				root_dir = function(fname)
+					return require("lspconfig.util").root_pattern(
+						"Makefile",
+						"configure.ac",
+						"configure.in",
+						"config.h.in",
+						"meson.build",
+						"meson_options.txt",
+						"build.ninja"
+					)(fname) or require("lspconfig.util").root_pattern(
+						"compile_commands.json",
+						"compile_flags.txt"
+					)(fname) or require("lspconfig.util").find_git_ancestor(fname)
+				end,
+				capabilities = {
+					offsetEncoding = { "utf-16" },
+				},
+				cmd = {
+					"clangd",
+					"--background-index",
+					"--clang-tidy",
+					"--header-insertion=iwyu",
+					"--completion-style=detailed",
+					"--function-arg-placeholders",
+					"--fallback-style={BasedOnStyle: Google, UseTab: Always, IndentWidth: 4, TabWidth: 4}",
+				},
+				init_options = {
+					usePlaceholders = true,
+					completeUnimported = true,
+					clangdFileStatus = true,
+				},
+			},
 		},
-		setup = {},
+		setup = {
+			clangd = function(_, opts)
+				local clangd_ext_opts = LazyVim.opts("clangd_extensions.nvim")
+				require("clangd_extensions").setup(
+					vim.tbl_deep_extend("force", clangd_ext_opts or {}, { server = opts })
+				)
+				return false
+			end,
+		},
 	},
 }
