ee7785a7f140e44242d9604123abca414de56e27
gaogao-qwq <gaogaoqwq@gmail.com>
2024-04-08 11:37:26 +0800

    Chore: Update edgy ui layout
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- lua/plugins/ui/edgy.lua
+++ lua/plugins/ui/edgy.lua
@@ -5,14 +5,14 @@ return {
 			bottom = {
 				{
 					ft = "toggleterm",
-					size = { height = 0.4 },
+					size = { height = 0.3 },
 					filter = function(buf, win)
 						return vim.api.nvim_win_get_config(win).relative == ""
 					end,
 				},
 				{
 					ft = "noice",
-					size = { height = 0.4 },
+					size = { height = 0.3 },
 					filter = function(buf, win)
 						return vim.api.nvim_win_get_config(win).relative == ""
 					end,
@@ -20,13 +20,13 @@ return {
 				{
 					ft = "lazyterm",
 					title = "LazyTerm",
-					size = { height = 0.4 },
+					size = { height = 0.3 },
 					filter = function(buf)
 						return not vim.b[buf].lazyterm_cmd
 					end,
 				},
 				"Trouble",
-				{ ft = "qf",                title = "QuickFix" },
+				{ ft = "qf", title = "QuickFix" },
 				{
 					ft = "help",
 					size = { height = 20 },
@@ -35,7 +35,7 @@ return {
 						return vim.bo[buf].buftype == "help"
 					end,
 				},
-				{ title = "Spectre",        ft = "spectre_panel",        size = { height = 0.4 } },
+				{ title = "Spectre", ft = "spectre_panel", size = { height = 0.3 } },
 				{ title = "Neotest Output", ft = "neotest-output-panel", size = { height = 15 } },
 			},
 			left = {
@@ -49,7 +49,7 @@ return {
 					open = function()
 						vim.api.nvim_input("<esc><space>e")
 					end,
-					size = { height = 0.5 },
+					size = { height = 0.5, width = 0.18 },
 				},
 				{ title = "Neotest Summary", ft = "neotest-summary" },
 				{
@@ -92,5 +92,5 @@ return {
 			},
 		}
 		return opts
-	end
+	end,
 }
