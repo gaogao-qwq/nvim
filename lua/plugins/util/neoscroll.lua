5a3daff8bf923d597eebcd2d06f0d58442b30fca
gaogao-qwq <gaogaoqwq@gmail.com>
2024-04-22 10:28:06 +0800

    Chore: Update neoscroll config
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- lua/plugins/util/neoscroll.lua
+++ lua/plugins/util/neoscroll.lua
@@ -1,15 +1,9 @@
 return {
 	"karb94/neoscroll.nvim",
 	opts = {
-		mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
-		hide_cursor = true,
-		stop_eof = true,
-		respect_scrolloff = false,
-		cursor_scrolls_alone = true,
-		easing_function = nil,
-		pre_hook = nil,
-		post_hook = nil,
-		performance_mode = false,
+		performance_mode = true,
 	},
+	config = function()
+		require("neoscroll").setup()
+	end,
 }
-
