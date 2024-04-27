1d901b0c85a665f6722f244a2bf9c50e39b75e06
gaogao-qwq <gaogaoqwq@gmail.com>
2024-04-27 16:20:55 +0800

    Chore: Use default lualine config
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -1,203 +1,91 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	opts = function()
-		local colors = {
-			bg       = '#202328',
-			fg       = '#bbc2cf',
-			yellow   = '#ECBE7B',
-			cyan     = '#008080',
-			darkblue = '#081633',
-			green    = '#98be65',
-			orange   = '#FF8800',
-			violet   = '#a9a1e1',
-			magenta  = '#c678dd',
-			blue     = '#51afef',
-			red      = '#ec5f67',
-		}
+		local lualine_require = require("lualine_require")
+		lualine_require.require = require
 
-		local conditions = {
-			buffer_not_empty = function()
-				return vim.fn.empty(vim.fn.expand('%:t')) ~= 1
-			end,
-			hide_in_width = function()
-				return vim.fn.winwidth(0) > 80
-			end,
-			check_git_workspace = function()
-				local filepath = vim.fn.expand('%:p:h')
-				local gitdir = vim.fn.finddir('.git', filepath .. ';')
-				return gitdir and #gitdir > 0 and #gitdir < #filepath
-			end,
-		}
+		local icons = require("lazyvim.config").icons
 
-		local config = {
+		vim.o.laststatus = vim.g.lualine_laststatus
+
+		return {
 			options = {
-				component_separators = '',
-				section_separators = '',
-				theme = {
-					normal = { nightfly = { fg = colors.fg, bg = colors.bg } },
-					inactive = { nightfly = { fg = colors.fg, bg = colors.bg } },
-				},
+				theme = "auto",
+				globalstatus = true,
+				disabled_filetypes = { statusline = { "dashboard", "alpha", "starter" } },
 			},
 			sections = {
-				lualine_a = {},
-				lualine_b = {},
-				lualine_y = {},
-				lualine_z = {},
-				lualine_c = {},
-				lualine_x = {},
+				lualine_a = { "mode" },
+				lualine_b = { "branch" },
+
+				lualine_c = {
+					LazyVim.lualine.root_dir(),
+					{
+						"diagnostics",
+						symbols = {
+							error = icons.diagnostics.Error,
+							warn = icons.diagnostics.Warn,
+							info = icons.diagnostics.Info,
+							hint = icons.diagnostics.Hint,
+						},
+					},
+					{ "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+					{ LazyVim.lualine.pretty_path() },
+				},
+				lualine_x = {
+        -- stylua: ignore
+        {
+          function() return require("noice").api.status.command.get() end,
+          cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+          color = LazyVim.ui.fg("Statement"),
+        },
+        -- stylua: ignore
+        {
+          function() return require("noice").api.status.mode.get() end,
+          cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+          color = LazyVim.ui.fg("Constant"),
+        },
+        -- stylua: ignore
+        {
+          function() return "  " .. require("dap").status() end,
+          cond = function () return package.loaded["dap"] and require("dap").status() ~= "" end,
+          color = LazyVim.ui.fg("Debug"),
+        },
+					{
+						require("lazy.status").updates,
+						cond = require("lazy.status").has_updates,
+						color = LazyVim.ui.fg("Special"),
+					},
+					{
+						"diff",
+						symbols = {
+							added = icons.git.added,
+							modified = icons.git.modified,
+							removed = icons.git.removed,
+						},
+						source = function()
+							local gitsigns = vim.b.gitsigns_status_dict
+							if gitsigns then
+								return {
+									added = gitsigns.added,
+									modified = gitsigns.changed,
+									removed = gitsigns.removed,
+								}
+							end
+						end,
+					},
+				},
+				lualine_y = {
+					{ "progress", separator = " ", padding = { left = 1, right = 0 } },
+					{ "location", padding = { left = 0, right = 1 } },
+				},
+				lualine_z = {
+					function()
+						return " " .. os.date("%R")
+					end,
+				},
 			},
-			inactive_sections = {
-				lualine_a = {},
-				lualine_b = {},
-				lualine_y = {},
-				lualine_z = {},
-				lualine_c = {},
-				lualine_x = {},
-			},
+			extensions = { "neo-tree", "lazy" },
 		}
-
-		local function ins_left(component)
-			table.insert(config.sections.lualine_c, component)
-		end
-
-		local function ins_right(component)
-			table.insert(config.sections.lualine_x, component)
-		end
-
-		ins_left {
-			function()
-				return '▊'
-			end,
-			color = { fg = colors.blue },
-			padding = { left = 0, right = 1 },
-		}
-
-		ins_left {
-			function()
-				return '󰣇'
-			end,
-			color = function()
-				local mode_color = {
-					n = colors.red,
-					i = colors.green,
-					v = colors.blue,
-					[''] = colors.blue,
-					V = colors.blue,
-					c = colors.magenta,
-					no = colors.red,
-					s = colors.orange,
-					S = colors.orange,
-					[''] = colors.orange,
-					ic = colors.yellow,
-					R = colors.violet,
-					Rv = colors.violet,
-					cv = colors.red,
-					ce = colors.red,
-					r = colors.cyan,
-					rm = colors.cyan,
-					['r?'] = colors.cyan,
-					['!'] = colors.red,
-					t = colors.red,
-				}
-				return { fg = mode_color[vim.fn.mode()] }
-			end,
-			padding = { right = 1 },
-		}
-
-		ins_left {
-			'filesize',
-			cond = conditions.buffer_not_empty,
-		}
-
-		ins_left {
-			'filetype',
-		}
-
-		ins_left {
-			'filename',
-			cond = conditions.buffer_not_empty,
-			color = { fg = colors.magenta, gui = 'bold' },
-		}
-
-		ins_left { 'location' }
-
-		ins_left { 'progress', color = { fg = colors.fg, gui = 'bold' } }
-
-		ins_left {
-			'diagnostics',
-			sources = { 'nvim_diagnostic' },
-			symbols = { error = ' ', warn = ' ', info = ' ' },
-			diagnostics_color = {
-				color_error = { fg = colors.red },
-				color_warn = { fg = colors.yellow },
-				color_info = { fg = colors.cyan },
-			},
-		}
-
-		ins_left {
-			function()
-				return '%='
-			end,
-		}
-
-		ins_left {
-			function()
-				local msg = 'No Active Lsp'
-				local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-				local clients = vim.lsp.get_active_clients()
-				if next(clients) == nil then
-					return msg
-				end
-				for _, client in ipairs(clients) do
-					local filetypes = client.config.filetypes
-					if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-						return client.name
-					end
-				end
-				return msg
-			end,
-			icon = ' LSP:',
-			color = { fg = '#ffffff', gui = 'bold' },
-		}
-
-		ins_right {
-			'o:encoding',
-			fmt = string.upper,
-			cond = conditions.hide_in_width,
-			color = { fg = colors.green, gui = 'bold' },
-		}
-
-		ins_right {
-			'fileformat',
-			fmt = string.upper,
-			icons_enabled = true,
-			color = { fg = colors.green, gui = 'bold' },
-		}
-
-		ins_right {
-			'branch',
-			icon = '',
-			color = { fg = colors.violet, gui = 'bold' },
-		}
-
-		ins_right {
-			'diff',
-			symbols = { added = ' ', modified = ' ', removed = ' ' },
-			diff_color = {
-				added = { fg = colors.green },
-				modified = { fg = colors.orange },
-				removed = { fg = colors.red },
-			},
-			cond = conditions.hide_in_width,
-		}
-
-		ins_right {
-			function()
-				return '▊'
-			end,
-			color = { fg = colors.blue },
-			padding = { left = 1 },
-		}
-		return config
 	end,
 }
