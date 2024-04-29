d6d4e62cef8c9b52b66aaac93fdaccd338bdde97
gaogao-qwq <gaogaoqwq@gmail.com>
2024-04-29 20:01:34 +0800

    Chore: Add sql to nvim-treesitter ensure_installed list
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- lua/plugins/treesitter/nvim-treesitter.lua
+++ lua/plugins/treesitter/nvim-treesitter.lua
@@ -1,14 +1,40 @@
 return {
-	'nvim-treesitter/nvim-treesitter',
+	"nvim-treesitter/nvim-treesitter",
 	opts = function()
 		if vim.loop.os_uname().sysname == "Darwin" then
-			require "nvim-treesitter.install".compilers = { "gcc-11" }
+			require("nvim-treesitter.install").compilers = { "gcc-11" }
 		end
 		return {
 			ensure_installed = {
-				"vim", "vimdoc", "bash", "c", "cpp", "make", "cmake", "javascript", "jsdoc", "typescript", "json",
-				"lua", "luadoc", "python", "java", "go", "rust", "toml", "html", "css", "scss", "vue", "dart", "org",
-				"markdown", "markdown_inline", "regex"
+				"vim",
+				"vimdoc",
+				"bash",
+				"c",
+				"cpp",
+				"make",
+				"cmake",
+				"just",
+				"javascript",
+				"jsdoc",
+				"typescript",
+				"json",
+				"lua",
+				"luadoc",
+				"python",
+				"java",
+				"go",
+				"rust",
+				"toml",
+				"html",
+				"css",
+				"scss",
+				"vue",
+				"dart",
+				"org",
+				"markdown",
+				"markdown_inline",
+				"regex",
+				"sql",
 			},
 			highlight = {
 				enable = true,
@@ -19,7 +45,7 @@ return {
 				enable = true,
 				extended_mode = true,
 				max_file_lines = nil,
-			}
+			},
 		}
 	end,
 }
