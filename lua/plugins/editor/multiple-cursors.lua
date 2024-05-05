ab51fbc05e0324453c7663d16f87d93f724edd62
gaogao-qwq <gaogaoqwq@gmail.com>
2024-05-05 21:56:54 +0800

    Feat: Add multiple-cursors dep
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

---
+++ lua/plugins/editor/multiple-cursors.lua
@@ -0,0 +1,13 @@
+return {
+	"brenton-leighton/multiple-cursors.nvim",
+	version = "*",
+	opts = {},
+	keys = {
+		{ "<C-M-j>", "<Cmd>MultipleCursorsAddDown<CR>", mode = { "n", "x" } },
+		{ "<C-Down>", "<Cmd>MultipleCursorsAddDown<CR>", mode = { "n", "i", "x" } },
+		{ "<C-M-k>", "<Cmd>MultipleCursorsAddUp<CR>", mode = { "n", "x" } },
+		{ "<C-Up>", "<Cmd>MultipleCursorsAddUp<CR>", mode = { "n", "i", "x" } },
+		{ "<C-LeftMouse>", "<Cmd>MultipleCursorsMouseAddDelete<CR>", mode = { "n", "i" } },
+		{ "<Leader>a", "<Cmd>MultipleCursorsAddMatches<CR>", mode = { "n", "x" } },
+	},
+}
