86a91f72857413d4e0b98ad5f8ca582bf067a5b1
gaogao-qwq <gaogaoqwq@gmail.com>
2024-05-13 10:59:56 +0800

    Chore: Update nvim-cmp configs
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- lua/plugins/lsp/nvim-cmp.lua
+++ lua/plugins/lsp/nvim-cmp.lua
@@ -6,7 +6,7 @@ return {
 		"hrsh7th/cmp-calc",
 		"onsails/lspkind.nvim",
 	},
-	opts = function(_, opts)
+	opts = function()
 		local has_words_before = function()
 			unpack = unpack or table.unpack
 			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
@@ -15,65 +15,72 @@ return {
 
 		local luasnip = require("luasnip")
 		local cmp = require("cmp")
+		local defaults = require("cmp.config.default")()
 		local lspkind = require("lspkind")
 
-		opts.mapping = vim.tbl_extend("force", opts.mapping, {
-			["<Tab>"] = cmp.mapping(function(fallback)
-				if cmp.visible() then
-					cmp.select_next_item()
-					-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-					-- they way you will only jump inside the snippet region
-				elseif luasnip.expand_or_jumpable() then
-					luasnip.expand_or_jump()
-				elseif has_words_before() then
-					cmp.complete()
-				else
-					fallback()
-				end
-			end, { "i", "s" }),
-			["<S-Tab>"] = cmp.mapping(function(fallback)
-				if cmp.visible() then
-					cmp.select_prev_item()
-				elseif luasnip.jumpable(-1) then
-					luasnip.jump(-1)
-				else
-					fallback()
-				end
-			end, { "i", "s" }),
-		})
-		opts.preselect = cmp.PreselectMode.None
-		opts.completion = {
-			completeopt = "noselect",
-		}
-		opts.window = {
-			completion = cmp.config.window.bordered(),
-			documentation = cmp.config.window.bordered(),
-		}
-		opts.formatting = {
-			format = lspkind.cmp_format({
-				mode = "symbol_text",
-				menu = ({
-					buffer = "[Buffer]",
-					nvim_lsp = "[LSP]",
-					luasnip = "[LuaSnip]",
-					nvim_lua = "[Lua]",
-					latex_symbols = "[Latex]",
-				})
-			})
-		}
-		opts.snippet = {
-			expand = function(args)
-				luasnip.lsp_expand(args.body)
-			end,
-		}
-		opts.sources = {
-			{ name = "calc" },
-			{ name = "orgmode" },
-			{ name = "nvim_lsp" },
-			{ name = "luasnip" },
-			{ name = "path" },
-			{ name = "buffer" },
-			{ name = "calc" },
+		return {
+			mapping = cmp.mapping.preset.insert({
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+						-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+						-- they way you will only jump inside the snippet region
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					elseif has_words_before() then
+						cmp.complete()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["C-f"] = cmp.mapping.scroll_docs(4),
+				["C-b"] = cmp.mapping.scroll_docs(-4),
+				["C-e"] = cmp.mapping.abort(),
+			}),
+			preselect = cmp.PreselectMode.None,
+			completion = {
+				completeopt = "menu,menuone,noinsert",
+			},
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
+			formatting = {
+				format = lspkind.cmp_format({
+					mode = "symbol_text",
+					menu = {
+						buffer = "[Buffer]",
+						nvim_lsp = "[LSP]",
+						luasnip = "[LuaSnip]",
+						nvim_lua = "[Lua]",
+						latex_symbols = "[Latex]",
+					},
+				}),
+			},
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			sources = cmp.config.sources({
+				{ name = "calc" },
+				{ name = "orgmode" },
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+				{ name = "path" },
+			}, {
+				{ name = "buffer" },
+			}),
+			sorting = defaults.sorting
 		}
 	end,
 }
