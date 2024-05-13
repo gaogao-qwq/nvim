772c5ff3417a6c16ce2baed343e29e3d65d05bd8
gaogao-qwq <gaogaoqwq@gmail.com>
2024-05-13 13:40:00 +0800

    Chore: Remove unnecessary bracket
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- lua/plugins/colorscheme/tokyonight.lua
+++ lua/plugins/colorscheme/tokyonight.lua
@@ -1,17 +1,15 @@
 return {
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {
-			transparent = true,
-			styles = {
-				sidebars = "transparent",
-				floats = "transparent",
-			},
+	"folke/tokyonight.nvim",
+	lazy = false,
+	priority = 1000,
+	opts = {
+		transparent = true,
+		styles = {
+			sidebars = "transparent",
+			floats = "transparent",
 		},
-		config = function()
-			vim.cmd([[colorscheme tokyonight-night]])
-		end,
 	},
+	config = function()
+		vim.cmd([[colorscheme tokyonight-night]])
+	end,
 }
