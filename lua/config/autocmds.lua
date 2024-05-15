c058d17ec99a411783ff6536fbaaa777e655203d
gaogao-qwq <gaogaoqwq@gmail.com>
2024-05-15 15:51:53 +0800

    Chore: Disable auto save autocmd for sql & mysql filetype
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,5 +1,10 @@
 vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
 	pattern = { "*" },
-	command = "silent! wall",
+	callback = function ()
+		if vim.bo.filetype == 'sql' or vim.bo.filetype == 'mysql' then
+			return
+		end
+		vim.api.nvim_command("silent! wall")
+	end,
 	nested = true,
 })
