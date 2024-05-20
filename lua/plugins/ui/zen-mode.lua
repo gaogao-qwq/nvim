b24d947b302e977c844ce613b2888949e992e43d
gaogao-qwq <gaogaoqwq@gmail.com>
2024-05-20 22:21:04 +0800

    Chore: Update zen-mode config
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- lua/plugins/ui/zen-mode.lua
+++ lua/plugins/ui/zen-mode.lua
@@ -5,9 +5,16 @@ return {
 		{
 			"<leader>uz",
 			function()
-				require("zen-mode").toggle({ window = { width = 0.85 } })
+				require("zen-mode").toggle({ window = { width = 0.75 } })
 			end,
 			desc = "Toggle Zen Mode",
+		},
+	},
+	opts = {
+		plugins = {
+			twilight = { enabled = false },
+			tmux = { enabled = true },
+			kitty = { enabled = true, font = "+2" },
 		},
 	},
 }
