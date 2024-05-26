0db02bb68e99ad4030efe8a62e619fd9b21d0738
gaogao-qwq <gaogaoqwq@gmail.com>
2024-05-26 16:33:13 +0800

    Chore: Enable Rainbow delimiters & indent line again...
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- lua/plugins/ui/rainbow-delimiters.lua
+++ lua/plugins/ui/rainbow-delimiters.lua
@@ -1,4 +1,18 @@
 return {
 	"HiPhish/rainbow-delimiters.nvim",
 	event = "VeryLazy",
+	opts = function()
+		local highlight = {
+			"RainbowRed",
+			"RainbowYellow",
+			"RainbowBlue",
+			"RainbowOrange",
+			"RainbowGreen",
+			"RainbowViolet",
+			"RainbowCyan",
+		}
+		return {
+			highlight = highlight
+		}
+	end
 }
