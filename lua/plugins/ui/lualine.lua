07058b0cbe8f9e5177084442a720c05a29d904b9
gaogao-qwq <gaogaoqwq@gmail.com>
2024-05-26 16:32:43 +0800

    Chore: Formatting...
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -33,24 +33,33 @@ return {
 					{ LazyVim.lualine.pretty_path() },
 				},
 				lualine_x = {
-        -- stylua: ignore
-        {
-          function() return require("noice").api.status.command.get() end,
-          cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
-          color = LazyVim.ui.fg("Statement"),
-        },
-        -- stylua: ignore
-        {
-          function() return require("noice").api.status.mode.get() end,
-          cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
-          color = LazyVim.ui.fg("Constant"),
-        },
-        -- stylua: ignore
-        {
-          function() return "  " .. require("dap").status() end,
-          cond = function () return package.loaded["dap"] and require("dap").status() ~= "" end,
-          color = LazyVim.ui.fg("Debug"),
-        },
+					{
+						function()
+							return require("noice").api.status.command.get()
+						end,
+						cond = function()
+							return package.loaded["noice"] and require("noice").api.status.command.has()
+						end,
+						color = LazyVim.ui.fg("Statement"),
+					},
+					{
+						function()
+							return require("noice").api.status.mode.get()
+						end,
+						cond = function()
+							return package.loaded["noice"] and require("noice").api.status.mode.has()
+						end,
+						color = LazyVim.ui.fg("Constant"),
+					},
+					{
+						function()
+							return "  " .. require("dap").status()
+						end,
+						cond = function()
+							return package.loaded["dap"] and require("dap").status() ~= ""
+						end,
+						color = LazyVim.ui.fg("Debug"),
+					},
 					{
 						require("lazy.status").updates,
 						cond = require("lazy.status").has_updates,
