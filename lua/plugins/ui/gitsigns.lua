19d55a4f31c4460e4b63ee83f66b07b75b995dd3
gaogao-qwq <gaogaoqwq@gmail.com>
2024-07-01 15:09:42 +0800

    Feat: Add nvim-scrollbar dep
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- lua/plugins/ui/gitsigns.lua
+++ lua/plugins/ui/gitsigns.lua
@@ -1,14 +1,17 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	event = "VeryLazy",
-	opts = {
-		signs = {
-			add = { text = '┃' },
-			change = { text = '┇' },
-			delete = { text = '║' },
-			topdelete = { text = '║'},
-			changedelete = { text = '║' },
-			untracked = { text = '┆' },
-		},
-	}
+	config = function()
+		require("gitsigns").setup({
+			signs = {
+				add = { text = "┃" },
+				change = { text = "┇" },
+				delete = { text = "║" },
+				topdelete = { text = "║" },
+				changedelete = { text = "║" },
+				untracked = { text = "┆" },
+			},
+		})
+		require("scrollbar.handlers.gitsigns").setup()
+	end,
 }
