66c9cb1326e461e4165f6aada12bf5d22a39ac69
gaogao-qwq <gaogaoqwq@gmail.com>
2024-07-08 00:28:00 +0800

    Chore: Remove deprecated api
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -47,13 +47,6 @@ opt.smartcase = true
 opt.termguicolors = true
 opt.signcolumn = "yes"
 
--- 诊断标志
-local signs = { Error = " ", Warn = " ", Hint = "󰌶 ", Info = " " }
-for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
-
 ---------------
 --- Neovide ---
 ---------------
