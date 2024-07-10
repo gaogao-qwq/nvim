5fb2f9412cdf2d3f51af5ef6ea17ab9d0b16732d
gaogao-qwq <gaogaoqwq@gmail.com>
2024-07-10 19:07:04 +0800

    Feat: Replace tiny-inline-diagnostic with diagflow
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

---
+++ lua/plugins/ui/diagflow.lua
@@ -0,0 +1,8 @@
+return {
+	"dgagn/diagflow.nvim",
+	event = "LspAttach",
+	opts = {
+		placement = "inline",
+		inline_padding_left = 4,
+	},
+}
--- lua/plugins/ui/tiny-inline-diagnostic.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"rachartier/tiny-inline-diagnostic.nvim",
-	event = "VeryLazy",
-	config = function()
-		require("tiny-inline-diagnostic").setup()
-	end,
-}
