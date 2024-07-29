38d449a66a623f7deed26abe041f6ae613faae42
gaogao-qwq <gaogaoqwq@gmail.com>
2024-07-30 00:54:00 +0800

    Chore: Remove unecessary explicit plugin define
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,5 @@
 return {
 	{ "mfussenegger/nvim-jdtls", event = "VeryLazy" },
-	{ "mrcjkb/rustaceanvim", event = "VeryLazy" },
-	{ "fatih/vim-go", event = "VeryLazy" },
 	{ "IndianBoy42/tree-sitter-just", event = "VeryLazy" },
 	{ "wakatime/vim-wakatime", event = "VeryLazy" },
 	{ "andweeb/presence.nvim", event = "VeryLazy" },
