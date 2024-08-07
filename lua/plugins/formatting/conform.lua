22cf5015f59ca09786ee327179b180ed6155b04f
gaogao-qwq <gaogaoqwq@gmail.com>
2024-08-07 10:35:11 +0800

    Fix: Fix conform update
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- lua/plugins/formatting/conform.lua
+++ lua/plugins/formatting/conform.lua
@@ -4,14 +4,12 @@ return {
 	opts = function()
 		vim.g.autoformat = false
 		local opts = {
-			-- LazyVim will use these options when formatting with the conform.nvim formatter
-			format = {
+			default_format_opts = {
 				timeout_ms = 3000,
 				async = false, -- not recommended to change
 				quiet = false, -- not recommended to change
-				lsp_fallback = true, -- not recommended to change
+				lsp_format = "fallback", -- not recommended to change
 			},
-			---@type table<string, conform.FormatterUnit[]>
 			formatters_by_ft = {
 				lua = { "stylua" },
 				sh = { "shfmt" },
@@ -19,11 +17,7 @@ return {
 				cpp = { "clang_format" },
 				c = { "clang_format" },
 				markdown = { "markdownlint" },
-				sql = { "sql_formatter" },
 			},
-			-- The options you set here will be merged with the builtin formatters.
-			-- You can also define any custom formatters here.
-			---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
 			formatters = {
 				injected = { options = { ignore_errors = true } },
 			},
