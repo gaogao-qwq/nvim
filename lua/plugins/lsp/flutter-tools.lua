10d6fef5ec0a63d3d3ed2a152bb8ad2e94def465
gaogao-qwq <gaogaoqwq@gmail.com>
2024-08-30 12:51:50 +0800

    Feat: Add flutter virtual_text color back
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- lua/plugins/lsp/flutter-tools.lua
+++ lua/plugins/lsp/flutter-tools.lua
@@ -8,6 +8,16 @@ return {
 		widget_guides = {
 			enabled = true,
 		},
-		fvm = true
-	}
+		fvm = true,
+		lsp = {
+			color = {
+				enabled = true,
+				background = false,
+				background_color = { r = 19, g = 17, b = 24 },
+				foreground = false,
+				virtual_text = true,
+				virtual_text_str = "■",
+			},
+		},
+	},
 }
