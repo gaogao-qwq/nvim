2bca6636495e465295c6475ba95859506eb60d62
gaogao-qwq <gaogaoqwq@gmail.com>
2024-08-30 11:01:13 +0800

    Fix: Fix luasnip issue
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- lua/plugins/lsp/luasnip.lua
+++ lua/plugins/lsp/luasnip.lua
@@ -1,8 +1,28 @@
 return {
 	"L3MON4D3/LuaSnip",
-	event = "VeryLazy",
+	lazy = true,
 	opts = {
 		history = true,
 		delete_check_events = "TextChanged",
 	},
+	config = function()
+		require("luasnip.loaders.from_lua").load({
+			paths = {
+				vim.fn.getcwd() .. "/snippets",
+				tostring(vim.fn.stdpath("config")),
+			},
+		})
+		require("luasnip.loaders.from_vscode").load({
+			paths = {
+				vim.fn.getcwd() .. "/snippets",
+				tostring(vim.fn.stdpath("config")),
+			},
+		})
+		require("luasnip.loaders.from_snipmate").load({
+			paths = {
+				vim.fn.getcwd() .. "/snippets",
+				tostring(vim.fn.stdpath("config")),
+			},
+		})
+	end,
 }
