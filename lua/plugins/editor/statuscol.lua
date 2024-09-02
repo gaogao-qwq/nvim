4e83c1fee4cbb2827ddd3fd4152ba14b1b8dc1bd
gaogao-qwq <gaogaoqwq@gmail.com>
2024-09-02 12:48:06 +0800

    Feat: Add fold level limitation
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- lua/plugins/editor/statuscol.lua
+++ lua/plugins/editor/statuscol.lua
@@ -3,12 +3,26 @@ return {
 	event = "VeryLazy",
 	config = function()
 		local builtin = require("statuscol.builtin")
+		local ffi = require("statuscol.ffidef")
+		local C = ffi.C
+
+		-- only show fold level up to this level
+		local fold_level_limit = 2
+		local function foldfunc(args)
+			local foldinfo = C.fold_info(args.wp, args.lnum)
+			if foldinfo.level > fold_level_limit then
+				return " "
+			end
+
+			return builtin.foldfunc(args)
+		end
+
 		require("statuscol").setup({
-			relculright = true,
+			relculright = false,
 			segments = {
-				{ text = { builtin.foldfunc }, click = "v:lua.ScFa" },
 				{ text = { "%s" }, click = "v:lua.ScSa" },
 				{ text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+				{ text = { foldfunc, " " }, condition = { true, builtin.not_empty }, click = "v:lua.ScFa" },
 			},
 		})
 	end,
