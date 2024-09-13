37bb1666488cda0498020488f529f5afc866ad4d
gaogao-qwq <gaogaoqwq@gmail.com>
2024-09-13 17:08:59 +0800

    feat: implement workspace specific ftplugin file
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -11,3 +11,28 @@ vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
 	end,
 	nested = true,
 })
+
+-- load workspace specific ftplugins
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+	pattern = { "*" },
+	callback = function()
+		local paths = vim.split(vim.fn.glob(vim.fn.getcwd() .. "/ftplugin/*.lua"), "\n", { trimempty = true })
+		local filetypes = {}
+		for _, path in pairs(paths) do
+			path = tostring(path:match("[^/]*.lua$"))
+			table.insert(filetypes, path:sub(0, #path - 4))
+		end
+
+		local ftplugins = {
+			filetypes = filetypes,
+			paths = paths,
+		}
+
+		for i, _ in ipairs(ftplugins.paths) do
+			-- print("filetype: " .. ftplugins.filetypes[i] .. " path: " .. ftplugins.paths[i] .. "\n")
+			if ftplugins.filetypes[i] == vim.bo.ft then
+				dofile(ftplugins.paths[i])
+			end
+		end
+	end,
+})
