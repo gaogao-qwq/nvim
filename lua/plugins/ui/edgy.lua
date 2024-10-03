7c0bb73d0aff48195b2c06ae37ad961aa8e7373a
gaogao-qwq <gaogaoqwq@gmail.com>
2024-10-04 01:23:59 +0800

    chore: update edgy config
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- lua/plugins/ui/edgy.lua
+++ lua/plugins/ui/edgy.lua
@@ -7,6 +7,7 @@ return {
 				{
 					ft = "noice",
 					size = { height = 0.3 },
+					animation = false,
 					filter = function(buf, win)
 						return vim.api.nvim_win_get_config(win).relative == ""
 					end,
@@ -14,6 +15,7 @@ return {
 				{
 					ft = "lazyterm",
 					title = "LazyTerm",
+					animation = false,
 					size = { height = 0.3 },
 					filter = function(buf)
 						return not vim.b[buf].lazyterm_cmd
@@ -22,24 +24,37 @@ return {
 				"Trouble",
 				{
 					ft = "help",
+					animation = false,
 					size = { height = 20 },
 					-- don't open help files in edgy that we're editing
 					filter = function(buf)
 						return vim.bo[buf].buftype == "help"
 					end,
 				},
-				{ title = "Spectre", ft = "spectre_panel", size = { height = 0.3 } },
-				{ title = "Neotest Output", ft = "neotest-output-panel", size = { height = 15 } },
+				{
+					title = "Spectre",
+					ft = "spectre_panel",
+					animation = false,
+					size = { height = 0.3 },
+				},
+				{
+					title = "Neotest Output",
+					ft = "neotest-output-panel",
+					animation = false,
+					size = { height = 15 },
+				},
 				{
 					ft = "dbout",
+					animation = false,
 					title = "DBOUT",
 					size = { height = 0.3 },
-				}
+				},
 			},
 			left = {
 				{
 					title = "Neo-Tree",
 					ft = "neo-tree",
+					animation = false,
 					filter = function(buf)
 						return vim.b[buf].neo_tree_source == "filesystem"
 					end,
@@ -50,64 +65,46 @@ return {
 					size = { height = 0.5, width = 0.18 },
 				},
 				{ title = "Neotest Summary", ft = "neotest-summary" },
-				{
-					title = "Neo-Tree Git",
-					ft = "neo-tree",
-					filter = function(buf)
-						return vim.b[buf].neo_tree_source == "git_status"
-					end,
-					pinned = true,
-					open = "Neotree position=right git_status",
-				},
-				{
-					title = "Neo-Tree Buffers",
-					ft = "neo-tree",
-					filter = function(buf)
-						return vim.b[buf].neo_tree_source == "buffers"
-					end,
-					pinned = true,
-					open = "Neotree position=top buffers",
-				},
-				"neo-tree",
 			},
 			right = {
 				{
 					ft = "dbui",
 					title = "DBUI",
+					animation = false,
 					size = { width = 0.2 },
 					pinned = true,
-					filter = function (buf)
+					filter = function(buf)
 						return vim.bo[buf].buftype == "nofile"
 					end,
-				}
+				},
 			},
 			keys = {
 				-- increase width
 				["<c-Right>"] = function(win)
 					win:resize("width", 2)
 				end,
-				["<m-L>"] = function (win)
+				["<m-L>"] = function(win)
 					win:resize("width", 2)
 				end,
 				-- decrease width
 				["<c-Left>"] = function(win)
 					win:resize("width", -2)
 				end,
-				["<m-H>"] = function (win)
+				["<m-H>"] = function(win)
 					win:resize("width", -2)
 				end,
 				-- increase height
 				["<c-Up>"] = function(win)
 					win:resize("height", 2)
 				end,
-				["<m-K>"] = function (win)
+				["<m-K>"] = function(win)
 					win:resize("height", 2)
 				end,
 				-- decrease height
 				["<c-Down>"] = function(win)
 					win:resize("height", -2)
 				end,
-				["<m-J>"] = function (win)
+				["<m-J>"] = function(win)
 					win:resize("height", -2)
 				end,
 			},
