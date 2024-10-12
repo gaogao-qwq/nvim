f90a1ee76c89455257c7e49aeac40c1a5f37223a
gaogao-qwq <gaogaoqwq@gmail.com>
2024-10-12 13:54:50 +0800

    chore: update lualine
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -1,21 +1,79 @@
+local mode_map = {
+	["n"] = "普通模式",
+	["no"] = "等待操作",
+	["nov"] = "等待操作",
+	["noV"] = "等待操作",
+	["no\22"] = "等待操作",
+	["niI"] = "普通模式-插入",
+	["niR"] = "普通模式-替换",
+	["niV"] = "普通模式-虚拟替换",
+	["nt"] = "终端-普通模式",
+	["v"] = "视觉模式",
+	["vs"] = "视觉模式-选择",
+	["V"] = "行-视觉模式",
+	["Vs"] = "行-视觉模式",
+	["\22"] = "块-视觉模式",
+	["\22s"] = "块-视觉模式",
+	["s"] = "选择模式",
+	["S"] = "行-选择模式",
+	["\19"] = "块-选择模式",
+	["i"] = "插入模式",
+	["ic"] = "插入模式",
+	["ix"] = "插入模式",
+	["R"] = "替换模式",
+	["Rc"] = "替换模式",
+	["Rx"] = "替换模式",
+	["Rv"] = "虚拟替换模式",
+	["Rvc"] = "虚拟替换模式",
+	["Rvx"] = "虚拟替换模式",
+	["c"] = "命令行模式",
+	["cr"] = "命令行模式",
+	["cv"] = "ex模式",
+	["ce"] = "ex模式",
+	["r"] = "回车确认",
+	["rm"] = "--更多--",
+	["r?"] = "确认",
+	["!"] = "shell模式",
+	["t"] = "终端模式",
+}
+
 return {
 	"nvim-lualine/lualine.nvim",
+	event = "VeryLazy",
+	init = function()
+		vim.g.lualine_laststatus = vim.o.laststatus
+		if vim.fn.argc(-1) > 0 then
+			-- set an empty statusline till lualine loads
+			vim.o.statusline = " "
+		else
+			-- hide the statusline on the starter page
+			vim.o.laststatus = 0
+		end
+	end,
 	opts = function()
+		-- PERF: we don't need this lualine require madness 🤷
 		local lualine_require = require("lualine_require")
 		lualine_require.require = require
 
-		local icons = require("lazyvim.config").icons
+		local icons = LazyVim.config.icons
 
 		vim.o.laststatus = vim.g.lualine_laststatus
 
-		return {
+		local opts = {
 			options = {
 				theme = "auto",
-				globalstatus = true,
-				disabled_filetypes = { statusline = { "dashboard", "alpha", "starter" } },
+				globalstatus = vim.o.laststatus == 3,
+				disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter" } },
 			},
 			sections = {
-				lualine_a = { "mode" },
+				lualine_a = {
+					{
+						"mode",
+						fmt = function(str)
+							return mode_map[vim.api.nvim_get_mode().mode] or "__"
+						end,
+					},
+				},
 				lualine_b = { "branch" },
 
 				lualine_c = {
@@ -33,38 +91,30 @@ return {
 					{ LazyVim.lualine.pretty_path() },
 				},
 				lualine_x = {
-					{
-						function()
-							return require("noice").api.status.command.get()
-						end,
-						cond = function()
-							return package.loaded["noice"] and require("noice").api.status.command.has()
-						end,
-						color = LazyVim.ui.fg("Statement"),
-					},
-					{
-						function()
-							return require("noice").api.status.mode.get()
-						end,
-						cond = function()
-							return package.loaded["noice"] and require("noice").api.status.mode.has()
-						end,
-						color = LazyVim.ui.fg("Constant"),
-					},
-					{
-						function()
-							return "  " .. require("dap").status()
-						end,
-						cond = function()
-							return package.loaded["dap"] and require("dap").status() ~= ""
-						end,
-						color = LazyVim.ui.fg("Debug"),
-					},
-					{
-						require("lazy.status").updates,
-						cond = require("lazy.status").has_updates,
-						color = LazyVim.ui.fg("Special"),
-					},
+            -- stylua: ignore
+            {
+              function() return require("noice").api.status.command.get() end,
+              cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+              color = function() return LazyVim.ui.fg("Statement") end,
+            },
+            -- stylua: ignore
+            {
+              function() return require("noice").api.status.mode.get() end,
+              cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+              color = function() return LazyVim.ui.fg("Constant") end,
+            },
+            -- stylua: ignore
+            {
+              function() return "  " .. require("dap").status() end,
+              cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
+              color = function() return LazyVim.ui.fg("Debug") end,
+            },
+            -- stylua: ignore
+            {
+              require("lazy.status").updates,
+              cond = require("lazy.status").has_updates,
+              color = function() return LazyVim.ui.fg("Special") end,
+            },
 					{
 						"diff",
 						symbols = {
@@ -88,13 +138,30 @@ return {
 					{ "progress", separator = " ", padding = { left = 1, right = 0 } },
 					{ "location", padding = { left = 0, right = 1 } },
 				},
-				lualine_z = {
-					function()
-						return " " .. os.date("%R")
-					end,
-				},
 			},
 			extensions = { "neo-tree", "lazy" },
 		}
+
+		-- do not add trouble symbols if aerial is enabled
+		-- And allow it to be overriden for some buffer types (see autocmds)
+		if vim.g.trouble_lualine and LazyVim.has("trouble.nvim") then
+			local trouble = require("trouble")
+			local symbols = trouble.statusline({
+				mode = "symbols",
+				groups = {},
+				title = false,
+				filter = { range = true },
+				format = "{kind_icon}{symbol.name:Normal}",
+				hl_group = "lualine_c_normal",
+			})
+			table.insert(opts.sections.lualine_c, {
+				symbols and symbols.get,
+				cond = function()
+					return vim.b.trouble_lualine ~= false and symbols.has()
+				end,
+			})
+		end
+
+		return opts
 	end,
 }
