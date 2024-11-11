28332675bfb03d9edd67d95dcb2b34b60cbfdd2c
gaogao-qwq <gaogaoqwq@gmail.com>
2024-11-11 11:01:34 +0800

    chore: window resize keybind
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -22,20 +22,25 @@ keymap.set("i", "<C-e>", "<End>")
 --- 正常模式 ---
 ----------------
 
+keymap.set("n", "<M-H>", [[<cmd>vertical resize -2<cr>]], { silent = true, nowait = true })
+keymap.set("n", "<M-J>", [[<cmd>horizontal resize +2<cr>]], { silent = true, nowait = true })
+keymap.set("n", "<M-K>", [[<cmd>horizontal resize -2<cr>]], { silent = true, nowait = true })
+keymap.set("n", "<M-L>", [[<cmd>vertical resize +2<cr>]], { silent = true, nowait = true })
+
 -- 取消高亮
 keymap.set("n", "<leader>nh", "<cmd>nohl<CR>")
 
 -- telescope
 local telescope = require('telescope.builtin')
-keymap.set('n', '<leader>ff', telescope.find_files, {}) -- 主键 + ff 查找文件
-keymap.set('n', '<leader>fo', telescope.oldfiles, {})   -- 主键 + fo 查找旧文件
-keymap.set('n', '<leader>fg', telescope.live_grep, {})  -- 主键 + fg 查找单词
-keymap.set('n', '<leader>fb', telescope.buffers, {})    -- 主键 + fb 查找标签
-keymap.set('n', '<leader>fh', telescope.help_tags, {})  -- 主键 + fh 查找帮助
-keymap.set('n', '<leader>ft', '<cmd>TodoTelescope<CR>') -- 主键 + ft 查找 TODO
+keymap.set("n", "<leader>ff", telescope.find_files, {}) -- 主键 + ff 查找文件
+keymap.set("n", "<leader>fo", telescope.oldfiles, {})   -- 主键 + fo 查找旧文件
+keymap.set("n", "<leader>fg", telescope.live_grep, {})  -- 主键 + fg 查找单词
+keymap.set("n", "<leader>fb", telescope.buffers, {})    -- 主键 + fb 查找标签
+keymap.set("n", "<leader>fh", telescope.help_tags, {})  -- 主键 + fh 查找帮助
+keymap.set("n", "<leader>ft", '<cmd>TodoTelescope<CR>') -- 主键 + ft 查找 TODO
 keymap.set(                                             -- 主键 + fb 从当前 buffer 的路径浏览文件
-	'n',
-	'<leader>fb',
+	"n",
+	"<leader>fb",
 	"<cmd>Telescope file_browser path=%:p:h select_buffer=true<CR>",
 	{}
 )
