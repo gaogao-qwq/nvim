3ce217c1ba9b74ef41dcbfabfcdf0c5ead20a29e
gaogao-qwq <gaogaoqwq@gmail.com>
2025-02-20 10:40:12 +0800

    chore: update go ftplugin
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -1,7 +1,7 @@
 local bo = vim.bo
 
-bo.tabstop = 4
-bo.shiftwidth = 4
+bo.tabstop = 2
+bo.shiftwidth = 2
 bo.expandtab = false
 
 vim.g.go_doc_keywordprg_enabled = 0 -- Disable keybind K to <Cmd>GoDoc<CR>
