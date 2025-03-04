40ab078379e592292a5921188b72df9ca15cae3c
gaogao-qwq <gaogaoqwq@gmail.com>
2025-03-04 16:58:45 +0800

    chore: sync deps
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- lua/plugins/editor/telescope-fzf-native.lua
+++ lua/plugins/editor/telescope-fzf-native.lua
@@ -1,6 +1,5 @@
 return {
-	'nvim-telescope/telescope-fzf-native.nvim',
+	"nvim-telescope/telescope-fzf-native.nvim",
 	event = "VeryLazy",
-	build =
-	'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+	build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
 }
