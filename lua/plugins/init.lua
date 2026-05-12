b2d1e29e27346b08a05b3d16078bcf639f462469
gaogao-qwq <gaogaoqwq@gmail.com>
2026-05-12 18:19:48 +0800

    chore: disable vim-wakatime for now
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,6 +1,6 @@
 return {
 	{ "mfussenegger/nvim-jdtls", event = "VeryLazy" },
-	{ "wakatime/vim-wakatime", event = "VeryLazy" },
+	-- { "wakatime/vim-wakatime", lazy = false },
 	{ "andweeb/presence.nvim", event = "VeryLazy" },
 	{ "norcalli/nvim-colorizer.lua", event = "VeryLazy" },
 }
